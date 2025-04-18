dfe45265576135a6dff4dfa22e9bf06c2b587418
Sunny Patel <sunnypatel2048@gmail.com>
2025-04-18 18:04:24 -0400

    added constructor dominance vis

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Viz1 from "./components/viz1/Viz1";
 import Viz2 from "./components/viz2/Viz2";
 import ConversionRates from "./components/conversion-rates/conversion-rates";
+import ConstructorDominance from "./components/constructor-dominance/constructor-dominance";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -11,13 +12,16 @@ function App() {
       <nav>
         <a href="/">Driver Journey Map</a> |{" "}
         <a href="/sankey">Qualifying vs Race Flow</a> |{" "}
-        <a href="/conversion-rates">Conversion Rates</a>
+        <a href="/conversion-rates">Conversion Rates</a> |{" "}
+        <a href="/constructor-dominance">Constructor Dominance</a>
       </nav>
       <Router>
         <Routes>
           <Route path="/" element={<Viz1 />} />
           <Route path="/sankey" element={<Viz2 />} />
           <Route path="/conversion-rates" element={<ConversionRates />} />
+          <Route path="/constructor-dominance" element={<ConstructorDominance />}
+          />
         </Routes>
       </Router>
     </>
